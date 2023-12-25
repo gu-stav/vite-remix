fe9930cbeafa1a7be635a016c725070d87b786c0
Gustav Hansen <gu@stav.dev>
2023-12-25 22:15:59 +0100

    opaque build folder

--- packages/studio/vite.config.js
+++ packages/studio/vite.config.js
@@ -1,8 +1,12 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [remix({
+    assetsBuildDirectory: resolve(process.env.ORIGINAL_CWD, 'build'),
+    serverBuildPath: resolve(process.env.ORIGINAL_CWD, 'build', 'server', 'index.js'),
+  })],
 
   resolve: {
     alias: [
