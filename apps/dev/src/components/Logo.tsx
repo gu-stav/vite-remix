0acb581121f0bd9383c26f8ac61245d060e138b3
Gustav Hansen <gu@stav.dev>
2023-12-25 21:15:27 +0100

    component overwrite test

---
+++ apps/dev/src/components/Logo.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export function Logo() {
+    return (
+        <strong>Custom Logo</strong>
+    );
+}
---
+++ apps/dev/studio.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "unding-studio-remix/config";
+
+import { Logo } from "./src/components/Logo";
+
+export default defineConfig({
+    Logo,
+    something: true,
+})
