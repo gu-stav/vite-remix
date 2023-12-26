56bf1de6325262cc10fcb42dbcc76ab17f08a0bf
Gustav Hansen <gu@stav.dev>
2023-12-26 19:12:19 +0100

    reorganize ui package

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,8 +1,7 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
