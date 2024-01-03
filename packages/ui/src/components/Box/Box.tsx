ce60e3c80bdcf8ce47655edde461f9a93349b582
Gustav Hansen <gu@stav.dev>
2024-01-03 15:17:57 +0100

    add logical padding props to Box, forward props from Flex -> Box

--- packages/ui/src/components/Box/Box.tsx
+++ packages/ui/src/components/Box/Box.tsx
@@ -10,9 +10,15 @@ export interface BoxProps {
     children: React.ReactNode;
     className?: string;
     display?: React.CSSProperties['display'];
+    paddingBlock?: React.CSSProperties['paddingBlock'];
+    paddingBlockEnd?: React.CSSProperties['paddingBlockEnd'];
+    paddingBlockStart?: React.CSSProperties['paddingBlockStart'];
+    paddingInline?: React.CSSProperties['paddingInline'];
+    paddingInlineEnd?: React.CSSProperties['paddingInlineEnd'];
+    paddingInlineStart?: React.CSSProperties['paddingInlineStart'];
 }
 
-export function Box({ asChild, children, display, className }: BoxProps) {
+export function Box({ asChild, children, display, className, paddingBlock, paddingBlockEnd, paddingBlockStart, paddingInline, paddingInlineEnd, paddingInlineStart }: BoxProps) {
     const Tag = asChild ? Slot : 'div';
 
     return <Tag className={clsx(
@@ -20,6 +26,30 @@ export function Box({ asChild, children, display, className }: BoxProps) {
 
         display === 'inline' && styles.displayInline,
         display === 'inline-block' && styles.displayInlineBlock,
+
+        paddingBlock === 1 && styles.paddingBlock1,
+        paddingBlock === 2 && styles.paddingBlock2,
+        paddingBlock === 3 && styles.paddingBlock3,
+
+        paddingBlockEnd === 1 && styles.paddingBlockEnd1,
+        paddingBlockEnd === 2 && styles.paddingBlockEnd2,
+        paddingBlockEnd === 3 && styles.paddingBlockEnd3,
+
+        paddingBlockStart === 1 && styles.paddingBlockStart1,
+        paddingBlockStart === 2 && styles.paddingBlockStart2,
+        paddingBlockStart === 3 && styles.paddingBlockStart3,
+
+        paddingInline === 1 && styles.paddingInline1,
+        paddingInline === 2 && styles.paddingInline2,
+        paddingInline === 3 && styles.paddingInline3,
+
+        paddingInlineEnd === 1 && styles.paddingInlineEnd1,
+        paddingInlineEnd === 2 && styles.paddingInlineEnd2,
+        paddingInlineEnd === 3 && styles.paddingInlineEnd3,
+
+        paddingInlineStart === 1 && styles.paddingInlineStart1,
+        paddingInlineStart === 2 && styles.paddingInlineStart2,
+        paddingInlineStart === 3 && styles.paddingInlineStart3,
 
         className,
     )}>
--- packages/ui/src/components/Flex/Flex.tsx
+++ packages/ui/src/components/Flex/Flex.tsx
@@ -10,10 +10,11 @@ import styles from './Flex.module.css';
 export interface FlexProps extends BoxProps {
     alignItems?: React.CSSProperties['alignItems'];
     direction?: React.CSSProperties['flexDirection'];
+    gap?: React.CSSProperties['gap'];
     justifyContent?: React.CSSProperties['justifyContent'];
 }
 
-export function Flex({ alignItems, asChild = false, children, className, direction = 'row', justifyContent }: FlexProps) {
+export function Flex({ alignItems, asChild = false, children, className, direction = 'row', gap, justifyContent, ...props }: FlexProps) {
     const Tag = asChild ? Slot : Box;
 
     return <Tag className={clsx(
@@ -27,13 +28,17 @@ export function Flex({ alignItems, asChild = false, children, className, directi
         direction === 'row' && styles.directionRow,
         direction === 'column' && styles.directionColumn,
 
+        gap === 1 && styles.gap1,
+        gap === 2 && styles.gap2,
+        gap === 3 && styles.gap3,
+
         justifyContent === 'start' && styles.justifyContentStart,
         justifyContent === 'center' && styles.justifyContentCenter,
         justifyContent === 'end' && styles.justifyContentEnd,
         justifyContent === 'stretch' && styles.justifyContentStretch,
 
         className
-    )}>
+    )} {...props}>
         {children}
     </Tag>
 }
